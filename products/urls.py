c278a87832a758ca514e9d3512704c687e663bc1
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-06-02 15:57:07 +0330

    add product view and category view function

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from .views import index , product_view , category_view
+from django.urls import path
+
+urlpatterns = [
+    path('',index),
+    path('product/<int:product_id>',product_view),
+    path('category/<slug:slug>',category_view)
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+from .models import Category,Product
 # Create your views here.
+
+def index(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()[:10]
+    category_response = ""
+    for c in categories:
+        category_response += f'<li><a href="/category/{c.slug}">{c.name}</a></li>'
+    category_response = f"<ul>{category_response}</ul>"
+    
+    product_response = ""
+    for p in products:
+        product_response += f'<li><a href="/product/{p.id}">{p.name}</a></li>'
+    product_response = f"<ul>{product_response}</ul>"
+    
+    return HttpResponse(f"""
+                        <html>
+                        <head><title>digikala</title></head>
+                        <body>
+                        <h1>the best site</h1>
+                        {category_response}
+                        {product_response}
+                        </body>
+                        </html>           
+                        """)
+
+def product_view(request,product_id):
+    try:
+        p = Product.objects.get(id = product_id)
+        return HttpResponse(f"""
+                        <html>
+                        <head><title>digikala</title></head>
+                        <body>
+                        <h1>{p.name}</h1>
+                        <h5>{p.en_name}</h5>
+                        <p>
+                        {p.description}
+                        </p>
+                        </body>
+                        </html>           
+                        """)
+    except Product.DoesNotExist:
+        return HttpResponse("404 Product not found")
+    
+
+def category_view(request,slug):
+    try:
+        c = Category.objects.get(slug = slug)
+        return HttpResponse(f"""
+                        <html>
+                        <head><title>digikala</title></head>
+                        <body>
+                        <h1>{c.name}</h1>
+                        <h5>{c.parent}</h5>
+                        <p>
+                        {c.description}
+                        </p>
+                        </body>
+                        </html>           
+                        """)
+    except Product.DoesNotExist:
+        return HttpResponse("404 Product not found")
