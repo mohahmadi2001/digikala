b741279ea1e3dc8859c134f3854a0208c101876c
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-07-04 09:29:01 +0330

    Seller class in sellers model

--- sellers/models.py
+++ sellers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Seller(models.Model):
+    name = models.CharField(_("Name"), max_length=50)
+    slug = models.SlugField(_("slug"))
+    
+
+    class Meta:
+        verbose_name = _("Seller")
+        verbose_name_plural = _("Sellers")
+
+    
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Seller_detail", kwargs={"pk": self.pk})
+    
