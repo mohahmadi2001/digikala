c154e544e03b4cf8564eb3a5e370f29e670d6d9c
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-07-05 09:54:47 +0330

    add seller admin

--- sellers/admin.py
+++ sellers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Seller
 # Register your models here.
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ['id','name','slug']
+    search_fields = ['slug','name']
