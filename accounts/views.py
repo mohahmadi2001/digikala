4212725cf7a93d472b812d5057517e206451be7e
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-07-07 15:56:40 +0330

    user_register_view function

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from accounts.forms import UserLogin
+from accounts.forms import UserLogin,RegisterUSer
 from django.contrib.auth import login
 # Create your views here.
 
@@ -20,3 +20,23 @@ def login_view(request):
         "accounts/login-view.html",
         context
     )
+
+def user_register_view(request):
+    if request.method == "GET":
+        form = UserLogin()
+    else:
+        form = UserLogin(request.POST, request=request)
+        if form.is_valid():
+            user = form.cleaned_data["user"]
+            user = form.save(commit=True)
+            login(request=request,user=user)
+            return redirect("")
+    context={
+        "form":form
+    }
+    return render(
+        request,
+        "accounts/login-view.html",
+        context
+    )
+    
